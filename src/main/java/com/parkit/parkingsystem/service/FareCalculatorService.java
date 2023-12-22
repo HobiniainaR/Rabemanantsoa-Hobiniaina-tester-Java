c7777693903ac2c31d867a9f8d57f2da2076d8d0
Hoby <rabhob@yahoo.fr>
2023-12-22 14:43:00 +0100

    Implémentation de  la fonctionnalité des 30 minutes gratuites

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,29 +3,56 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.util.Date;
+
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket) {
+        if (ticket.getInTime() == null || ticket.getOutTime() == null) {
+            throw new IllegalArgumentException("InTime or OutTime is missing");
         }
 
-       long inTime = ticket.getInTime().getTime();
-       long outTime = ticket.getOutTime().getTime();
+        Date inTime = ticket.getInTime();
+        Date outTime = ticket.getOutTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-       double duration =((double) outTime -(double) inTime)/(60*60*1000);
+        long durationInMillis = outTime.getTime() - inTime.getTime();
+        double duration = durationInMillis / (60.0 * 60.0 * 1000.0);
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+        if (duration < 0) {
+            throw new IllegalArgumentException("OutTime is before InTime");
+        }
+
+        // Calculate fare based on parking type
+        switch (ticket.getParkingSpot().getParkingType()) {
+            case CAR:
+                calculateCarFare(ticket, duration);
                 break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+            case BIKE:
+                calculateBikeFare(ticket, duration);
                 break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unknown parking type");
+        }
+    }
+
+    private void calculateCarFare(Ticket ticket, double duration) {
+        if (duration < 0.5) {
+            ticket.setPrice(0);
+        } else if (duration <= 1) {
+            ticket.setPrice(duration*Fare.CAR_RATE_PER_HOUR);
+        } else {
+            ticket.setPrice(duration*Fare.CAR_RATE_PER_HOUR);
+        }
+    }
+
+    private void calculateBikeFare(Ticket ticket, double duration) {
+        if (duration < 0.5) {
+            ticket.setPrice(0);
+        } else if (duration <= 1) {
+            ticket.setPrice(duration*Fare.BIKE_RATE_PER_HOUR);
+        } else {
+            ticket.setPrice(duration*Fare.BIKE_RATE_PER_HOUR);
         }
     }
 }
--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -124,4 +124,41 @@ public class FareCalculatorServiceTest {
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
     }
 
+
+@Test
+public void calculateFareCarWithLessThan30minutesParkingTime(){
+	// GIVEN
+    Date inTime = new Date();
+    inTime.setTime( System.currentTimeMillis() - (  25 * 60 * 1000) );//25 minutes parking time should give free parking fare
+    Date outTime = new Date();
+    ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);    
+    ticket.setInTime(inTime);
+    ticket.setOutTime(outTime);
+    ticket.setParkingSpot(parkingSpot);
+    
+    // WHEN
+    fareCalculatorService.calculateFare(ticket);
+    
+    // THEN
+    assertEquals(( 0 *Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
+}
+
+@Test
+public void calculateFareBikeWithLessThan30minutesParkingTime(){
+	// GIVEN
+    Date inTime = new Date();
+    inTime.setTime( System.currentTimeMillis() - (  25 * 60 * 1000) );//25 minutes parking time should give free parking fare
+    Date outTime = new Date();
+    ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+    ticket.setInTime(inTime);
+    ticket.setOutTime(outTime);
+    ticket.setParkingSpot(parkingSpot);
+    
+    // WHEN
+    fareCalculatorService.calculateFare(ticket);
+    
+    // THEN
+    assertEquals( (0 * Fare.BIKE_RATE_PER_HOUR), ticket.getPrice());
+}
+
 }
